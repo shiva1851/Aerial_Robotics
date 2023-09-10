b6bd4af70b04b0b78085dfda033ab411ba45aae4
SUSMIT NEOGI <120775551+shiva1851@users.noreply.github.com>
2023-09-10 12:10:33 +0530

    Add files via upload

---
+++ Week02_Assignment/PD_Controller/runsim.m
@@ -0,0 +1,12 @@
+close all;
+clear;
+
+z_des = 20
+% Given trajectory generator
+trajhandle = @(t) fixed_set_point(t, z_des);
+
+% This is your controller
+controlhandle = @controller;
+
+% Run simulation with given trajectory generator and controller
+[t, z] = height_control(trajhandle, controlhandle);
